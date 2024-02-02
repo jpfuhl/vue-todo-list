b68e41d186866e81c2d742f9e0a48d5736890606
Joko@Home <joachim.pfuhl@yahoo.de>
2024-02-02 23:29:10 +0100

    added html to views and router-transitions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { Transition, watch, ref } from "vue";
+import { useRoute, RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+
+const route = useRoute();
+const transitionName = ref("");
+
+watch(
+  () => route.path,
+  (to, from) => {
+    transitionName.value =
+      to === "/archive" && from === "/" ? "slide-left" : "slide-right";
+    console.log(transitionName.value);
+  }
+);
+
+// maybe fetch here and save in pinia store
 </script>
 
 <template>
   <Navbar />
   <main>
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition :name="transitionName" mode="out-in">
+        <component :is="Component"></component>
+      </Transition>
+    </RouterView>
   </main>
 </template>
 
 <style scoped>
+/* Slide and fade in from right */
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: all 0.5s ease-in-out;
+}
 
+.slide-left-enter-from {
+  opacity: 0;
+  transform: translateX(25%);
+}
+.slide-left-leave-to {
+  opacity: 0;
+  transform: translateX(-25%);
+}
+
+.slide-left-enter-to,
+.slide-left-leave-from,
+.slide-right-enter-to,
+.slide-right-leave-from {
+  opacity: 1;
+}
+
+.slide-right-enter-from {
+  opacity: 0;
+  transform: translate(-25%);
+}
+
+.slide-right-leave-to {
+  opacity: 0;
+  transform: translateX(25%);
+}
 </style>
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,29 +1,81 @@
 <script setup lang="ts">
-const tasks: String[] = ['task1'];
+import { ref, onMounted } from "vue";
+
+type Task = {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
+};
+
+const tasks = ref<Task[]>([]);
+
+onMounted(() => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => {
+      json.forEach((task: Task) => {
+        if (tasks.value.length === 12) return;
+        tasks.value.push(task);
+      });
+    });
+});
+
+const input = ref("");
+
+const addTask = () => {
+  const newTask: Task = {
+    id: tasks.value.length + 1,
+    userId: 1,
+    title: input.value,
+    completed: false,
+  };
+  tasks.value.push(newTask);
+  input.value = "";
+};
 </script>
 
 <template>
-  <section>
-    <h2>Add new Task</h2>
-    <form>
-      <input type="text" placeholder="Enter a new task..." />
-      <button type="submit">+ ADD</button>
-    </form>
-  </section>
+  <div>
+    <section class="max-width-container">
+      <h2>Add new Task</h2>
+      <form @submit.prevent="addTask">
+        <input type="text" placeholder="Enter a new task..." v-model="input" />
+        <button type="submit">+ ADD</button>
+      </form>
+    </section>
 
-  <section>
-    <h2>Your open Tasks <span>({{ tasks.length }})</span></h2>
-    <input type="text" placeholder="Search for a task..." />
+    <section class="max-width-container">
+      <h2>
+        Your open Tasks <span>({{ tasks.length }})</span>
+      </h2>
+      <input type="text" placeholder="Search for a task..." />
 
-    <div v-if="tasks.length">
-      <div v-for="(task, index) in tasks" :key="index">
-        <input type="checkbox">
-        <p>{{ task }}</p>
+      <div class="task-container" v-if="tasks.length">
+        <div v-for="(task, index) in tasks" :key="task.id">
+          <input type="checkbox" v-model="task.completed" />
+          <p>{{ task.title }}</p>
+        </div>
       </div>
-    </div>
 
-    <div v-else>
-      <p>No tasks available</p>
-    </div>
-  </section>
+      <div v-else>
+        <p>No tasks available</p>
+      </div>
+    </section>
+  </div>
 </template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  margin-top: 3rem;
+}
+
+.task-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1.5rem;
+}
+</style>
