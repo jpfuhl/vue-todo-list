2563bfe0f1aaf358812c8785d8a809df69f6463e
Joko@Home <joachim.pfuhl@yahoo.de>
2024-02-02 17:40:39 +0100

    sketched basic html for 'my tasks'

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const tasks: String[] = ['task1'];
+</script>
 
 <template>
-  <h1>MyTasks</h1>
+  <section>
+    <h2>Add new Task</h2>
+    <form>
+      <input type="text" placeholder="Enter a new task..." />
+      <button type="submit">+ ADD</button>
+    </form>
+  </section>
+
+  <section>
+    <h2>Your open Tasks <span>({{ tasks.length }})</span></h2>
+    <input type="text" placeholder="Search for a task..." />
+
+    <div v-if="tasks.length">
+      <div v-for="(task, index) in tasks" :key="index">
+        <input type="checkbox">
+        <p>{{ task }}</p>
+      </div>
+    </div>
+
+    <div v-else>
+      <p>No tasks available</p>
+    </div>
+  </section>
 </template>
