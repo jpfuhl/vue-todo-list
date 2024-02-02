629661496ec1b0c07690b4faca4dce9a473c2840
Joko@Home <joachim.pfuhl@yahoo.de>
2024-02-02 17:07:47 +0100

    fix basic routing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <Navbar />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">My Tasks</RouterLink>
+      <RouterLink to="/archive">Archive</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+</style>
