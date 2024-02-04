ce5bbab1314135fbcab80c89a0a83c612099cfe5
Joko@Home <joachim.pfuhl@yahoo.de>
2024-02-04 18:05:20 +0100

    made sticky navbar and initials/full name upon breakpoints

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import { RouterLink } from "vue-router";
-
-const user = {
-  name: "Max Mustermann",
-  initials: "M. M.",
-};
-</script>
-
 <template>
   <header>
     <nav class="max-width-container">
@@ -16,16 +7,31 @@ const user = {
       </div>
       <div class="user-container">
         <font-awesome-icon icon="fa-solid fa-user" class="icon" />
-        <p>{{ user.name }}</p>
-        <!-- <p>M. M.</p> -->
+        <p class="user-name">{{ user.name }}</p>
+        <p class="user-initials">M. M.</p>
       </div>
     </nav>
   </header>
 </template>
 
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
+const user = {
+  name: "Max Mustermann",
+  initials: "M. M.",
+};
+</script>
+
 <style scoped>
 header {
-  background-color: #f2f2f2;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  background-color: #f9f9f9;
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.1);
 }
 
 nav {
@@ -37,11 +43,11 @@ nav {
 
 .nav-link-container {
   display: flex;
-  gap: 3rem;
+  gap: 2.5rem;
 }
 
 .nav-link {
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: bold;
   text-decoration: none;
   color: rgb(0, 0, 0, 0.5);
@@ -64,6 +70,42 @@ nav {
 
 .icon {
   font-size: 1.25rem;
-  color: rgb(0, 0, 0);
+  color: rgb(0, 0, 0, 0.5);
+  transition: color 0.3s ease-in-out;
+}
+
+.icon:hover {
+  color: blue;
+}
+
+p {
+  font-size: 1.25rem;
+  color: rgb(0, 0, 0, 0.5);
+}
+
+.user-initials {
+  display: block;
+}
+
+.user-name {
+  display: none;
+}
+
+@media (min-width: 640px) {
+  .nav-link-container {
+    gap: 3.5rem;
+  }
+
+  .nav-link {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  .user-initials {
+    display: none;
+  }
+  .user-name {
+    display: block;
+  }
 }
 </style>
